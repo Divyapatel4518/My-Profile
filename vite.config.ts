5cac60106244df4cee67b6c2ddeebc57a8e582fc
Divyapatel4518 <Divya.patel4518@gmail.com>
2025-12-11 21:02:17 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
@@ -6,11 +6,14 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/Profile/',        // <-- your GitHub repo name
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
+     base: '/My-Profile/', // Your GitHub repo name
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
     },
+
+    
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
