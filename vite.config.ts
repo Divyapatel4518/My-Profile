fb370f7fed8f2750323e34d613ffc3c002041eb8
Divyapatel4518 <Divya.patel4518@gmail.com>
2025-12-12 19:03:24 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "node:path";
 
-// ESM fix for __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
