1149f14094c53aa6039be1a5e47a4e011f3bd1b8
Divyapatel4518 <Divya.patel4518@gmail.com>
2025-12-12 18:57:25 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-
-  return {
-    
-base: "/My-Profile/",
-
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
+export default defineConfig({
+  base: "/My-Profile/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
     },
-
-    plugins: [react()],
-
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './'),
-      },
-    },
-
-    build: {
-      rollupOptions: {
-        external: ['lucide-react', 'framer-motion'], // fix build errors
-      },
-      outDir: 'dist',
-    },
-  };
+  },
 });
