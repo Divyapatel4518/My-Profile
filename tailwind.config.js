714d325171223af602af21f42b9cee2cef6b7052
Divyapatel4518 <Divya.patel4518@gmail.com>
2025-12-13 10:49:21 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 
-
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          DEFAULT: "#C7A76C",
+          dark: "#A68A55",
+        },
+        grey: {
+          light: "#F5F5F3",
+          dark: "#2b2b2b",
+        },
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        serif: ["Playfair Display", "serif"],
+      },
+    },
+  },
   plugins: [],
-}
+};
+
