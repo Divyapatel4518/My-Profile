5dbaa1d09cff50febd67ad88f2734b69fa37af5e
Divyapatel4518 <Divya.patel4518@gmail.com>
2025-12-13 11:42:46 +1000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
